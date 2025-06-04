78c46534878a11c536f5968f4df6d7aab748b64a
dm-thedeveloper <thedeveloperdm786@gmail.com>
2025-06-04 12:31:55 +0500

    set remove_dirnmae

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
-import path from "path";
 
 const app = express();
 
@@ -15,8 +14,59 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 
 // Default Routes
+const defaultHTML  = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>User CURD & Auth Server</title>
+  </head>
+  <style>
+    /* boilerplate */
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+  
+
+    body {
+      min-height: 100vh;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-image: url("./binary.gif");
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+
+    h1{
+      color: transparent;
+      font-size: 70px;
+      text-align: center;
+       background-image: url("./text\ gif.gif");
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-clip: text;
+      font-weight: 900;
+      -webkit-text-stroke: 2px rgba(255, 255, 255);
+      font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+    }
+  </style>
+  <body>
+    <h1> User CURD & <br> Auth Secrver </h1>
+  </body>
+</html>
+
+`
 app.get("/", (req, res, _) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.send(defaultHTML)
 });
 
 // Route Importing
